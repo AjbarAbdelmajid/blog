347d931dd9d2f453d5d617af653a02d056106155
mjid <ajbareabdlmjid@gmail.com>
2019-09-23 07:15:44 +0100

    update the image link

--- src/Form/Extension/ImageTypeExtension.php
+++ src/Form/Extension/ImageTypeExtension.php
@@ -16,7 +16,7 @@ class ImageTypeExtension extends AbstractTypeExtension{
         return [FileType::class];
     }
 
-    public function buildView(FormView $view, FormInterface $form){
+    public function buildView(FormView $view, FormInterface $form,  array $options){
         $parentData = $form->getParent()->getData();
         
         // check if there is an uploaded image
